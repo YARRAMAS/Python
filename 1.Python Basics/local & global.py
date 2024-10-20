d96af8378a52265d355a203f64bf10285ceb4039
YARRAMAS <146420288+YARRAMAS@users.noreply.github.com>
2024-10-20 19:55:48 +0530

    Delete 1.Python Basics directory

--- 1.Python Basics/local & global.py
+++
@@ -1,9 +0,0 @@
-x = 50
-
-def my_function():
-    # Local variable with the same name
-    x = 10
-    print("Local variable x:", x)
-
-my_function()
-print("Global variable x:", x) 
--- 1.Python Basics/single & multiple lines.py
+++
@@ -1,9 +0,0 @@
-print("single line comments starts with #")
-# This is the single line comment
-print("Multple lines starts with double quotes or single quotes")
-""" This is the multple line comment:--
-This is first line.
-This is second line.
-This is third line.
-..............
-"""
